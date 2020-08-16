585f50b7416fad0c1b19245b8376bf80ee0c7740
rafayetn <rafayet@nimblehq.co>
2020-08-16 22:15:24 +0600

    Add faulty commit to test Danger implementation.

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders app name SURVEYOR', () => {
   const { getByText } = render(<App />);
-  const appName = getByText(/SURVEYOR/i);
+  const appName = getByText(/SURVEYOR_TEST/i);
   expect(appName).toBeInTheDocument();
 });
