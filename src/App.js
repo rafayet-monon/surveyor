358c94cedf94d6c632b556bc032b9f4e2b7d6e59
rafayetn <rafayet@nimblehq.co>
2020-08-20 11:37:30 +0600

    Added route related components, authContext and authentication folder structures.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.scss';
+import { BrowserRouter as Router } from 'react-router-dom';
+import RouteList from './routes';
+import ScrollToTop from './routes/scrollToTop';
+import { AuthProvider } from './contexts/authContext';
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <p>SURVEYOR</p>
-      </header>
-    </div>
+    <Router>
+      <ScrollToTop>
+        <AuthProvider>
+          <div className="app">
+            <RouteList />
+          </div>
+        </AuthProvider>
+      </ScrollToTop>
+    </Router>
   );
 }
 
---
+++ src/contexts/authContext.js
@@ -0,0 +1,65 @@
+import React, { createContext, useEffect, useReducer } from 'react';
+
+const AuthContext = createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'AUTH': {
+      const user = action.payload.attributes;
+      const token = action.payload.attributes.token;
+      localStorage.setItem('user', user);
+      localStorage.setItem('token', token);
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: user,
+        token: token,
+      };
+    }
+    case 'REFRESH': {
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: localStorage.getItem('user'),
+        token: localStorage.getItem('token'),
+      };
+    }
+    case 'LOGOUT': {
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+        token: null,
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    let token = localStorage.getItem('token');
+
+    if (token && !state.isAuthenticated) {
+      dispatch({ type: 'REFRESH' });
+    }
+  });
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export { AuthProvider, AuthContext };
