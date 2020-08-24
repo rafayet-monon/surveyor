eb41093f304734647ba338d838e53804cc503635
rafayetn <rafayet@nimblehq.co>
2020-08-24 08:28:33 +0600

    Remove unnecessary scroll to top.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import './App.scss';
 import { BrowserRouter as Router } from 'react-router-dom';
 import RouteList from './routes';
-import ScrollToTop from './routes/scrollToTop';
 import { AuthProvider } from './contexts/authContext';
 
 function App() {
   return (
     <Router>
-      <ScrollToTop>
-        <AuthProvider>
-          <RouteList />
-        </AuthProvider>
-      </ScrollToTop>
+      <AuthProvider>
+        <RouteList />
+      </AuthProvider>
     </Router>
   );
 }
