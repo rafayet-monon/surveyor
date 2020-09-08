97b9102fc097a29764f3e8e6e4aea7a6307606f5
rafayet-monon <rafayet.monon@gmail.com>
2020-09-08 19:06:26 +0600

    Added rem for font-sizes. fix button class name.

--- src/components/ButtonSubmit/index.js
+++ src/components/ButtonSubmit/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ButtonSubmit = ({ label }) => {
   return (
     <div className="button-submit">
-      <button type="submit" className="btn button-submit__btn--white">
+      <button type="submit" className="btn button-submit__button--white">
         { label }
       </button>
     </div>
