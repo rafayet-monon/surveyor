1a561631ab1d8f1f7177a1cde26ed3a172c00c2a
rafayet-monon <rafayet.monon@gmail.com>
2020-09-08 15:09:28 +0600

    Fixed color, mixin file, button class naming.

--- src/containers/Login/page.js
+++ src/containers/Login/page.js
@@ -9,15 +9,13 @@ import Form from './form';
 const Page = () => {
   const logoLabel = 'Sign in to Nimble';
   return (
-    <div>
-      <BackgroundLayout backgroundImage={ backgroundImage }>
-        <div className="container container-login">
-          <Logo label={ logoLabel } />
+    <BackgroundLayout backgroundImage={ backgroundImage }>
+      <div className="container container-login">
+        <Logo label={ logoLabel } />
 
-          <Form />
-        </div>
-      </BackgroundLayout>
-    </div>
+        <Form />
+      </div>
+    </BackgroundLayout>
   );
 };
 
