c973dedda2f3eee0b7ceb2bc0f6303efd69f0f3f
rafayet-monon <rafayet.monon@gmail.com>
2020-09-15 23:55:38 +0600

    Changed tests of auth screens

--- src/tests/containers/ForgotPassword/page.test.js
+++ src/tests/containers/ForgotPassword/page.test.js
@@ -1,8 +1,24 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
 import Page from 'containers/ForgotPassword/page';
-import AuthPage from 'tests/shared_examples/authPage';
+import backgroundImage from 'images/auth_background.png';
 
 describe('When visited the Login Page', () => {
   const logoLabel =
     'Enter your email to receive instructions for resetting your password.';
-  AuthPage(logoLabel, Page);
+  it('shows a background image', () => {
+    const { getByLabelText } = render(<Page />);
+    const imageDiv = getByLabelText('background-layout-image');
+
+    expect(imageDiv).toHaveStyle(`background-image: url(${backgroundImage})`);
+  });
+
+  it('shows a logo label', () => {
+    const { getByText } = render(<Page />);
+    const labelText = getByText(logoLabel);
+
+    expect(labelText).toBeInTheDocument();
+  });
 });
--- src/tests/containers/Login/page.test.js
+++ src/tests/containers/Login/page.test.js
@@ -1,7 +1,24 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
 import Page from 'containers/Login/page';
-import AuthPage from 'tests/shared_examples/authPage';
+import backgroundImage from 'images/auth_background.png';
+
 
 describe('When visited the Login Page', () => {
   const logoLabel = 'Sign in to Nimble';
-  AuthPage(logoLabel, Page);
+  it('shows a background image', () => {
+    const { getByLabelText } = render(<Page />);
+    const imageDiv = getByLabelText('background-layout-image');
+
+    expect(imageDiv).toHaveStyle(`background-image: url(${backgroundImage})`);
+  });
+
+  it('shows a logo label', () => {
+    const { getByText } = render(<Page />);
+    const labelText = getByText(logoLabel);
+
+    expect(labelText).toBeInTheDocument();
+  });
 });
