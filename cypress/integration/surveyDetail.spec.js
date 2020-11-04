40d723116fd7ee858e4e2c624d575d0e383009ed
rafayet-monon <rafayet.monon@gmail.com>
2020-11-04 13:35:38 +0600

    Fix UI testing error.

--- cypress/integration/surveyDetail.spec.js
+++ cypress/integration/surveyDetail.spec.js
@@ -15,7 +15,7 @@ describe('The survey detail page', () => {
   it('goes to survey list if back arrow clicked', function () {
     cy.get('.survey-detail__arrow-left').click()
     cy.url().should('include', '/')
-    cy.get('div[class="home-header__left"]').find('img').should('be.visible');
+    cy.get('div[class="home-header__logo"]').find('img').should('be.visible');
     cy.get('div[class="survey-list__navigation"]').find('button').should('be.visible');
   })
 })
