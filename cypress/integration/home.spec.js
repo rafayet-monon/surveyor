7578d8daff8ee6d3e957b5b061c6936f01059ae8
rafayet-monon <rafayet.monon@gmail.com>
2020-11-12 16:46:53 +0600

    [TASK-14] Fix UI test name.

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -12,7 +12,7 @@ describe('The Home Page', () => {
     cy.get('.react-loading-skeleton').should('exist');
   })
 
-  it('shows loading screen after successful log', function () {
+  it('shows home screen after successful login', function () {
     cy.url().should('include', '/')
     cy.get('div[class="home-header__logo"]').find('img').should('be.visible');
     cy.get('div[class="survey-list__navigation"]').find('button').should('be.visible');
