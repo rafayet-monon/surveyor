569f110a082520d62cc62ef67781cd027189ef17
rafayet-monon <rafayet.monon@gmail.com>
2020-12-15 17:16:14 +0600

    [TASK-29] Created sidebar.

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+
+import userImage from 'images/user-placeholder.jpg';
+
+const Sidebar = () => {
+  const [sidebar, setSidebar] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+
+  const logout = () => {
+    localStorage.clear();
+
+    window.location.href = '/login';
+  };
+
+  return (
+    <div className="sidebar">
+      <img
+        src={ userImage }
+        alt="USER"
+        className="sidebar__user-image"
+        onClick={ showSidebar }
+        onKeyDown={ showSidebar }
+        role="presentation"
+      />
+
+      <nav
+        className={
+          sidebar
+            ? 'sidebar__nav-menu sidebar__nav-menu-active'
+            : 'sidebar__nav-menu'
+        }
+      >
+        <ul className="sidebar__nav-menu-items">
+          <li className="sidebar__nav-menu-information">
+            <span className="sidebar__nav-menu-username">Mai</span>
+            <span className="sidebar__nav-menu-image">
+              <img
+                src={ userImage }
+                alt="USER"
+                className="sidebar__user-image"
+                onClick={ showSidebar }
+                onKeyDown={ showSidebar }
+                role="presentation"
+              />
+            </span>
+          </li>
+          <li className="sidebar__nav-menu-line" />
+          <li
+            className="sidebar__nav-menu-logout"
+            onClick={ logout }
+            onKeyDown={ logout }
+            role="presentation"
+          >
+            Logout
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/screens/Home/header.js
+++ src/screens/Home/header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
+import Sidebar from 'components/Sidebar';
 import logo from 'images/logo_white.png';
-import userImage from 'images/user-placeholder.jpg';
 
 const Header = () => {
   return (
@@ -10,7 +10,7 @@ const Header = () => {
         <img src={ logo } alt="NIMBLE" className="home-header__site-logo" />
       </div>
       <div className="home-header__menu">
-        <img src={ userImage } alt="USER" className="home-header__user-image" />
+        <Sidebar />
       </div>
     </div>
   );
