0bb6ceb38a273c94d5a73c7efc4d7294ec2068a9
rafayet-monon <rafayet.monon@gmail.com>
2020-12-17 11:43:04 +0600

    [TASK-18] Fix reviews

--- src/components/NetPromotingScore/index.js
+++ src/components/NetPromotingScore/index.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 const NetPromotingScore = ({ start, end }) => {
   const startScore = start || 1;
   const endScore = end || 10;
-  const [nps, setNps] = useState(Math.ceil((startScore + endScore) / 2));
+  const [nps, setNps] = useState(0);
   const scoreElements = Array.from(
     { length: endScore - startScore + 1 },
     (v, k) => k + startScore
@@ -19,11 +19,7 @@ const NetPromotingScore = ({ start, end }) => {
             <li
               role="presentation"
               key={ index }
-              className={
-                ratingValue === scoreElements.length
-                  ? 'np-score__number-label np-score__number-label--no-border'
-                  : 'np-score__number-label'
-              }
+              className="np-score__number-label"
               onClick={ () => setNps(score) }
               onKeyDown={ () => setNps(score) }
             >
@@ -39,15 +35,16 @@ const NetPromotingScore = ({ start, end }) => {
             </li>
           );
         }) }
-        <div className="np-score__identifier">
-          <div className="np-score__low-score">
-            <p>Not at all Likely</p>
-          </div>
-          <div className="np-score__high-score">
-            <p>Extremely Likely</p>
-          </div>
-        </div>
       </ul>
+
+      <div className="np-score__identifier">
+        <div className="np-score__low-score">
+          <p>Not at all Likely</p>
+        </div>
+        <div className="np-score__high-score">
+          <p>Extremely Likely</p>
+        </div>
+      </div>
     </div>
   );
 };
