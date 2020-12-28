f68c48862f64bc75e401d7e6e4d2d529594d6d9c
rafayet-monon <rafayet.monon@gmail.com>
2020-12-28 08:20:49 +0600

    [TASK-20] Fix refresh token UI tests.

--- cypress/integration/expiredToken.spec.js
+++ cypress/integration/expiredToken.spec.js
@@ -7,10 +7,20 @@ describe('Expired Login', () => {
     cy.login()
   })
 
-  it('redirects to login page if token is invalid', function () {
+  it('refreshes auth token if token is invalid', function () {
     cy.get('div[class="home-header__logo"]').find('img').should('be.visible');
     cy.getCookie('authorization_token').then(() => {
       localStorage.setItem('authorization_token', null);
+    });
+    cy.reload()
+    cy.url().should('include', '/')
+  })
+
+  it('redirects to login page if refresh token is invalid', function () {
+    cy.get('div[class="home-header__logo"]').find('img').should('be.visible');
+    cy.getCookie('authorization_token').then(() => {
+      localStorage.setItem('authorization_token', null);
+      localStorage.setItem('refresh_token', null);
     });
     cy.reload()
     cy.url().should('include', '/login')
