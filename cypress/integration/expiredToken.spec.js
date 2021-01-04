98528c295c34f6968321c2b0acf74f0ffdbbe6dd
rafayet-monon <rafayet.monon@gmail.com>
2021-01-04 15:43:44 +0600

    [TASK-28] Used shorthand suntax for functions

--- cypress/integration/expiredToken.spec.js
+++ cypress/integration/expiredToken.spec.js
@@ -7,7 +7,7 @@ describe('Expired Login', () => {
     cy.login()
   })
 
-  it('refreshes auth token if token is invalid', function () {
+  it('refreshes auth token if token is invalid', () => {
     cy.getCookie('authorization_token').then(() => {
       localStorage.setItem('authorization_token', null);
     });
@@ -15,7 +15,7 @@ describe('Expired Login', () => {
     cy.url().should('include', '/')
   })
 
-  it('redirects to login page if refresh token is invalid', function () {
+  it('redirects to login page if refresh token is invalid', () => {
     cy.getCookie('authorization_token').then(() => {
       localStorage.setItem('authorization_token', null);
       localStorage.setItem('refresh_token', null);
