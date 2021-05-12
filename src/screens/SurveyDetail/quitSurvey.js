005e8612f85ad4a06bc606217a5e772bc8791abe
rafayet-monon <rafayet.monon@gmail.com>
2021-05-12 23:59:57 +0600

    [TASK-22-23] Add thanks and outro component

---
+++ src/screens/SurveyDetail/quitSurvey.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+
+import ReactDOM from 'react-dom';
+import { useHistory } from 'react-router-dom';
+
+import closeIcon from 'images/close-button-white.png';
+
+const QuitSurvey = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const history = useHistory();
+  const returnToHome = () => history.push('/');
+
+  return (
+    <div>
+      <button className="button" onClick={ handleShow }>
+        <img src={ closeIcon } alt="close" />
+      </button>
+
+      { show
+        ? ReactDOM.createPortal(
+            <React.Fragment>
+              <div
+                className="quit-survey-modal__container"
+                aria-modal
+                aria-hidden
+                tabIndex={ -1 }
+                role="dialog"
+              >
+                <div className="quit-survey-modal__content">
+                  <div className="quit-survey-modal__header">Warning!</div>
+                  <div className="quit-survey-modal__body">
+                    Are you sure you want to quit the survey?
+                  </div>
+                  <div className="quit-survey-modal__footer">
+                    <button
+                      type="submit"
+                      className="button button--primary quit-survey-modal__yes-button"
+                      onClick={ returnToHome }
+                    >
+                      Yes
+                    </button>
+
+                    <button
+                      type="submit"
+                      className="button button--primary quit-survey-modal__cancel-button"
+                      onClick={ handleClose }
+                    >
+                      Cancel
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </React.Fragment>,
+            document.body
+          )
+        : null }
+    </div>
+  );
+};
+
+export default QuitSurvey;
