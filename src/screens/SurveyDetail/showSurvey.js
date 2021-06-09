7490dc3542fa9ad203ff3b68ca80ccdff6da3b85
rafayet-monon <rafayet.monon@gmail.com>
2021-06-09 18:34:24 +0600

    [TASK-27] Add surveysubmit functionality

--- src/screens/SurveyDetail/showSurvey.js
+++ src/screens/SurveyDetail/showSurvey.js
@@ -1,15 +1,24 @@
 import React, { useContext } from 'react';
 
+import { DetailsContext } from 'contexts/details';
+import { SurveyAnswerProvider } from 'contexts/surveyAnswer';
 import { SurveyStatusContext } from 'contexts/surveyStatus';
 import Detail from 'screens/SurveyDetail/detail';
 import Questions from 'screens/SurveyDetail/question';
 
 const ShowSurvey = () => {
   const surveyStatusContext = useContext(SurveyStatusContext);
+  const detailsContext = useContext(DetailsContext);
 
   return (
     <React.Fragment>
-      { surveyStatusContext.state.started ? <Questions /> : <Detail /> }
+      { surveyStatusContext.state.started ? (
+        <SurveyAnswerProvider surveyId={ detailsContext.surveyDetail.data.id }>
+          <Questions />
+        </SurveyAnswerProvider>
+      ) : (
+        <Detail />
+      ) }
     </React.Fragment>
   );
 };
